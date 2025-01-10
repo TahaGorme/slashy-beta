8fb9aff63117b625063cf8a1d58e7501eb619723
Taha Gorme <63650975+TahaGorme@users.noreply.github.com>
2025-01-10 14:30:27 +0000

    auto adventure

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const CONFIG = {
   BOT_ID: "270904126974590976", // Discord bot ID to interact with
   PLAY_IN_DMS: false, // Play in DMs instead of server
   CHANNEL_ID: "796729044468367370", // Channel ID for interaction (leave empty if PLAY_IN_DMS is true)
-  DEV_MODE: false, // Debug mode flag (set to true for additional logging)
+  DEV_MODE: true, // Debug mode flag (set to true for additional logging)
   WEBSITE_USERNAME: "slashy", // Website username
   WEBSITE_PASSWORD: "slashy", // Website
   API_ENDPOINT: "http://localhost:5000/predict", // API endpoint for image prediction
   POST_MEMES_PLATFORMS: ["reddit", "tiktok"], // Platform to post memes or RANDOM
   IS_FISHING_ENABLED: true, // Enable fishing minigame
   IS_STREAMING_ENABLED: true, // Enable streaming minigame
+  IS_ADVENTURE_ENABLED: true, // Enable adventure minigame
   AUTOUSE: [
     {
       name: "Lucky Horseshoe",
@@ -32,19 +33,18 @@ const CONFIG = {
     },
   ],
   DELAYS: {
-    MIN_COMMAND: 1000, // Minimum delay between commands
-    MAX_COMMAND: 2000, // Maximum delay between commands
+    MIN_COMMAND: 100, // Minimum delay between commands
+    MAX_COMMAND: 200, // Maximum delay between commands
     SHORT_BREAK: [3000, 6000], // Short break duration
     LONG_BREAK: [30000, 60000], // Long break duration
   },
   COOLDOWNS: {
-    highlow: 2500,
-    beg: 11000,
+    highlow: 2000,
+    beg: 10000,
     crime: 30000,
-    hunt: 30000,
-    postmemes: 2300,
-    search: 2300,
-    hunt: 11000,
+    postmemes: 2000,
+    search: 2000,
+    hunt: 10000,
   },
   SEARCH_LOCATIONS: [
     "Basement",
@@ -66,6 +66,34 @@ const CONFIG = {
     "Vacuum",
     "Washer",
   ],
+  ADVENTURE_WEST: {
+    "A lady next to a broken down wagon is yelling for help.": "Ignore Her",
+    "A snake is blocking your path. What do you want to do?": "Wait",
+    "A stranger challenges you to a quick draw. What do you want to do?":
+      "Decline",
+    "Someone is getting ambushed by bandits!": "Ignore them",
+    "Someone on the trail is lost and asks you for directions.": "Ignore them",
+    "You bump into someone near the horse stables. They challenge you to a duel":
+      "Run away",
+    "You come across a saloon with a poker game going on inside. What do you want to do?":
+      "Join",
+    "You entered the saloon to rest from the journey. What do you want to do?":
+      "Play the piano",
+    "You find a dank cellar with an old wooden box": "Ignore it",
+    "You find an abandoned mine. What do you want to do?": "Explore",
+    "You found a stray horse. What do you want to do?": "Feed",
+    "You get on a train and some bandits decide to rob the train. What do you do?":
+      "Don't hurt me!",
+    "You see some bandits about to rob the local towns bank. What do you do?":
+      "Stop them",
+    "You wander towards an old abandoned mine.": "Go in",
+    "You're dying of thirst. Where do you want to get water?": "Cactus",
+    "You're riding on your horse and you get ambushed. What do you do?":
+      "Run away",
+    "Your horse sees a snake and throws you off. What do you do?":
+      "Find a new horse",
+    "__**WANTED:**__": "Billy Bob Jr.",
+  },
 };
 
 //commands which will prevent the bot from executing other commands
@@ -164,6 +192,7 @@ async function slashy(token) {
     isStreamStillRunning: true, // Flag to indicate if the stream is still running
     thingsToBuy: [], // Items to buy
     latestBuyQuantity: 0, // Latest buy quantity
+    streamingFlag: false,
   };
 
   // Command management
@@ -402,13 +431,16 @@ async function slashy(token) {
     if (CONFIG.IS_STREAMING_ENABLED) {
       CommandManager.addCommand("stream");
     }
+    if (CONFIG.IS_ADVENTURE_ENABLED) {
+      CommandManager.addCommand("adventure");
+    }
     try {
-      if (!CONFIG.IS_FISHING_ENABLED) return;
       CommandManager.channel = CONFIG.PLAY_IN_DMS
         ? await (await client.users.fetch(CONFIG.BOT_ID)).createDM()
         : await client.channels.fetch(CONFIG.CHANNEL_ID);
-      CommandManager.addCommand("fish catch");
       initializeBot();
+      if (!CONFIG.IS_FISHING_ENABLED) return;
+      CommandManager.addCommand("fish catch");
 
       // check if the bot is able to fish
       // auto unstuck the bot if it is stuck
@@ -503,6 +535,20 @@ async function slashy(token) {
       await handleFishingCooldown(message);
     }
 
+    //handle adventure
+    if (
+      message?.embeds[0]?.title?.includes(
+        "choose items you want to bring along"
+      )
+    ) {
+      await message.clickButton({ X: 0, Y: 1 });
+    }
+
+    // handle adventure
+    if (message.interaction.commandName.includes("adventure")) {
+      handleAdventure(message);
+    }
+
     // Handle fishing minigame
     if (message?.embeds[0]?.title?.includes("Fishing...")) {
       if (!CONFIG.IS_FISHING_ENABLED) return;
@@ -527,7 +573,7 @@ async function slashy(token) {
           "What game do you want to stream"
         )
       ) {
-        if (State.isBotBusy) return;
+        if (State.streamingFlag) return;
         const trendingGame = await fetch(
           `https://api.dankmemer.tools/trending`
         ).then((res) => res.text());
@@ -541,11 +587,14 @@ async function slashy(token) {
             ].value;
         }
         console.log(`[INFO] Streaming ${streamGame}`);
+        State.streamingFlag = true;
         State.isBotBusy = true;
         await message.selectMenu(streamMenu, [streamGame]);
-        message.clickButton({ X: 0, Y: 1 });
+        await message.clickButton({ X: 0, Y: 1 });
 
+        State.streamingFlag = false;
         State.isBotBusy = false;
+
         setTimeout(() => {
           message.clickButton({ X: randomInt(0, 2), Y: 0 });
           State.lastStreamTimestamp = Date.now();
@@ -578,6 +627,53 @@ async function slashy(token) {
       )
     ) {
       await handleFishSelling(message);
+    }
+  }
+  async function handleAdventure(message) {
+    let description = message.embeds[0].description;
+
+    if (message.embeds[0].author.name.includes("Adventure Summary")) {
+      //get label of button
+      let buttonLabel = message.components[0].components[0].label;
+      //Adventure again in 22 minute
+      if (buttonLabel.includes("dventure again in")) {
+        let time = buttonLabel.match(/\d+/);
+        setTimeout(() => {
+          CommandManager.addCommand("adventure");
+        }, time * 60000 + randomInt(10000, 20000));
+      }
+    }
+    if (!description) return;
+
+    console.log(`[INFO] Adventure: ${description}`);
+
+    if (description?.includes("catch one of them")) {
+      await message.clickButton({ X: randomInt(0, 2), Y: 0 });
+      return message.clickButton({ X: 1, Y: 1 });
+    }
+
+    if (!message.components[1]) {
+      return message.clickButton({ X: 1, Y: 0 });
+    }
+
+    //check description from CONFIG.ADVENTURE_WEST
+    let action = Object.keys(CONFIG.ADVENTURE_WEST).find((key) =>
+      description.includes(key)
+    );
+    action = CONFIG.ADVENTURE_WEST[action];
+    console.log(`[INFO] Adventure: ${description} => ${action}`);
+    if (action) {
+      // find the button with the label equal to the action
+      let buttonIndex = message.components[0].components.findIndex((btn) => {
+        return btn.label
+          .toLowerCase()
+          .trim()
+          .includes(action.toLowerCase().trim());
+      });
+
+      console.log(`[INFO] Button index: ${buttonIndex}`);
+      await message.clickButton({ X: buttonIndex, Y: 0 });
+      await message.clickButton({ X: 1, Y: 1 });
     }
   }
   async function handleInventory(message) {
@@ -726,15 +822,41 @@ async function slashy(token) {
         await message.clickButton({ X: randomInt(0, 2), Y: 0 });
         State.lastStreamTimestamp = Date.now();
 
-        const interval = setInterval(() => {
+        const interval = setInterval(async () => {
           if (Date.now() - State.lastStreamTimestamp > 1000 * 60 * 12) {
             if (!State.isStreamStillRunning) clearInterval(interval);
-            message.clickButton({ X: randomInt(0, 2), Y: 0 });
+            await message.clickButton({ X: randomInt(0, 2), Y: 0 });
             State.lastStreamTimestamp = Date.now();
+            console.log("[INFO] Stream is still running");
           }
         }, 20000);
       } else {
         await message.clickButton({ X: 0, Y: 0 });
+      }
+    }
+
+    //handle adventure
+    if (message.interaction.commandName.includes("adventure")) {
+      handleAdventure(message);
+    }
+
+    if (
+      message.embeds[0]?.description?.includes(
+        "You can start another adventure"
+      )
+    ) {
+      //You can start another adventure at <t:1736519912:t>
+      const time =
+        message.embeds[0].description.match(/<t:(\d+):t>/)[1] * 1000 -
+        Date.now() +
+        randomInt(1000, 2000);
+      setTimeout(() => CommandManager.addCommand("adventure"), time);
+    }
+    // adventure
+    if (message?.embeds[0]?.author?.name?.includes("Choose an Adventure")) {
+      await message.selectMenu(message.components[0].components[0], ["west"]);
+      if (!message.components[0].components[1].disabled) {
+        await message.clickButton({ X: 0, Y: 1 });
       }
     }
 
